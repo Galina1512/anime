2eff964b8ceac661d14e95383f6b29fc000f333d
Galina1512 <Kostina13@gmail.com>
2025-08-30 21:04:40 +0300

    lesson_1

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector('.search-model');
+const modalBtn = document.querySelector('.icon_search');
+const modalClose = modal.querySelector('.search-close-switch');
+const searchInput = modal.querySelector('.search-model-form')
+const input = modal.querySelector('input');
+console.log(input.value);
+
+modalBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+})
+
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+
+input.addEventListener('keydown', () => {
+   console.log(input.value);
+});
