d9831657bc6c8ef56d33bb4864c1bc1f8a43b28b
Galina1512 <Kostina13@gmail.com>
2025-08-31 21:37:22 +0300

    lesson_2

--- js/mainData.js
+++ js/mainData.js
@@ -1,10 +1,12 @@
 const mainData = () => {
-    fetch('./db.json')
+
+    fetch('https://sfo.cloud.appwrite.io/v1/storage/buckets/68b478570014ec5489b8/files/68b4796b0002432e0796/view?project=68b470350000f6671ada&mode=admin')
+    // fetch('./db.json')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data.anime)
+            console.log(data)
         })
 }
 mainData();
